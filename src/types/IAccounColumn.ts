a68f1a4ba4c4b3f172fc3c10b30883b0c9f6eede
Polina Tcaciuc <polinatcaciuc@gmail.com>
2025-06-29 19:32:21 +0300

    feat: Добавить интерфейс IAccounColumn

---
+++ src/types/IAccounColumn.ts
@@ -0,0 +1,24 @@
+export interface AccountColumn {
+  name: string
+  value: string
+  ref: string
+  content: string
+  label?: string
+  width?: string | number
+  classes?: string[]
+  props?: {
+    placeholder?: string
+    type?: string
+    clearable?: boolean
+    required?: boolean
+    maxlength?: string
+    showPassword?: boolean
+    filterable?: boolean
+    optionLabel?: string
+    optionKey?: string
+    optionValue?: string
+    options?: Array<Record<string, unknown>>
+    symbolId?: string
+    fixed?: 'left' | 'right'
+  }
+}
