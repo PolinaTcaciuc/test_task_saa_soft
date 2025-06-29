04346da2326cf8dff61c75f765e401b806c06165
Polina Tcaciuc <polinatcaciuc@gmail.com>
2025-06-29 19:24:01 +0300

    feat: Реализовать компонент InformationHint

---
+++ src/components/Base/InformationHint.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import BaseIcon from './BaseIcon.vue'
+</script>
+
+<template>
+  <div class="information-hint">
+    <BaseIcon symbolId="question" />
+    <slot />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.information-hint {
+  display: flex;
+  gap: 12px;
+  align-items: center;
+  padding: 4px 8px;
+  background-color: $blue;
+}
+</style>
