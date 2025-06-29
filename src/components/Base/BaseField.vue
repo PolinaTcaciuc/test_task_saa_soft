ce18a76f2c3872b30a24e8b3ad579c2f8e379010
Polina Tcaciuc <polinatcaciuc@gmail.com>
2025-06-29 19:59:11 +0300

    feat: Реализовать компонент ввода

---
+++ src/components/Base/BaseField.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+
+const props = defineProps({
+  warning: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+const model = defineModel()
+const emit = defineEmits(['afterEdit', 'update:warning'])
+
+const localWarning = ref(props.warning)
+
+function endEditing() {
+  emit('afterEdit')
+}
+
+watch(
+  () => props.warning,
+  (val) => {
+    localWarning.value = val
+  }
+)
+
+function setWarning(val: boolean) {
+  localWarning.value = val
+  emit('update:warning', val)
+}
+
+defineExpose({
+  setWarning,
+})
+</script>
+
+<template>
+  <div class="editable">
+    <el-input
+      v-model="model"
+      v-bind="$attrs"
+      :class="{ warning: localWarning }"
+      @blur="endEditing"
+    />
+  </div>
+</template>
