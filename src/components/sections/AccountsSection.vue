fd4a6b06fefb375ea453b134c7578c434a5f3970
Polina Tcaciuc <polinatcaciuc@gmail.com>
2025-06-29 19:45:10 +0300

    feat: Реализовать секцию с учетными записями

---
+++ src/components/sections/AccountsSection.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup>
+import { useAccountsStore } from '@/stores/accounts'
+import BaseButton from '@/components/Base/BaseButton.vue'
+import InformationHint from '@/components/Base/InformationHint.vue'
+const { addNewAccount } = useAccountsStore()
+</script>
+
+<template>
+  <section class="accounts container">
+    <div class="accounts__top">
+      <div class="accounts__head">
+        <h1>Учетные записи</h1>
+        <BaseButton @click="addNewAccount">+</BaseButton>
+      </div>
+      <InformationHint> Для указания нескольких меток для одной пары логин/пароль используйте разделитель ; </InformationHint>
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss">
+.accounts {
+  padding-top: 40px;
+  padding-bottom: 40px;
+
+  &__top {
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+    align-items: start;
+    margin-bottom: 24px;
+  }
+
+  &__head {
+    display: flex;
+    gap: 12px;
+    align-items: center;
+  }
+}
+</style>
