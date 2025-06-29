700079b43c44079360e519aa1da6ee78104e0ad7
Polina Tcaciuc <polinatcaciuc@gmail.com>
2025-06-29 18:49:39 +0300

    feat: Реализовать компонент для отображения иконок

---
+++ src/components/Base/BaseIcon.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+interface Props {
+  symbolId: string
+  size?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  size: '24px',
+})
+const fullIconPath = computed(() => `/icons-sprite.svg#${props.symbolId}`)
+</script>
+
+<template>
+  <svg
+    aria-hidden="true"
+    :width="size"
+    :height="size"
+  >
+    <use :xlink:href="fullIconPath"></use>
+  </svg>
+</template>
+
+<style lang="scss" scoped>
+svg {
+  flex-shrink: 0;
+  color: inherit;
+}
+</style>
