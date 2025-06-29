646992e350a1f82c25434b2a43a7d2dea8a30ad7
Polina Tcaciuc <polinatcaciuc@gmail.com>
2025-06-29 19:53:15 +0300

    feat: Реализовать компонент выпадающего списка

---
+++ src/components/Base/BaseSelect.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+interface Props {
+  options: Array<Record<string, unknown>>
+  optionLabel: string
+  optionKey: string
+  optionValue: string
+}
+
+defineProps<Props>()
+const model = defineModel()
+</script>
+
+<template>
+  <el-select
+    v-model="model"
+    class="editable"
+  >
+    <el-option
+      v-for="item in options"
+      :key="item[optionKey]"
+      :label="item[optionLabel]"
+      :value="item[optionValue]"
+    />
+  </el-select>
+</template>
