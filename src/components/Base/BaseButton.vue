57726e2e94608412a0520205e78d5e8082b61397
Polina Tcaciuc <polinatcaciuc@gmail.com>
2025-06-29 18:50:14 +0300

    feat: Реализовать кнопку

---
+++ src/components/Base/BaseButton.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import BaseIcon from './BaseIcon.vue'
+
+interface Props {
+  symbolId?: string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <el-button v-bind="$attrs">
+    <BaseIcon
+      v-if="symbolId"
+      :symbolId="symbolId"
+    />
+    <template v-if="$slots.default">
+      <slot></slot>
+    </template>
+  </el-button>
+</template>
